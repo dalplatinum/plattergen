20fc1344b8fe4e224275fecd09146709538ac97e
dalplatinum <92800232+dalplatinum@users.noreply.github.com>
2021-10-26 11:46:00 +0100

    Add files via upload

--- test.js
+++ test.js
@@ -11,6 +11,10 @@
 // Individual letters mode
 // Image mode
 // *DONE* When particle count = 0 clear sceen, stop loop
+// Quality/number options
+// make the code more efficient
+// 	put some sort of debug counter on the screen that counts operations or something
+// 	might want to put each 'style' into it's own particle type?  would reduce the amount of 'ifs'
 
 window.onload = function() {
 	
@@ -18,7 +22,10 @@ window.onload = function() {
 	const queryString = window.location.search;
 	//console.log(queryString);
 	const urlParams = new URLSearchParams(queryString);
-	const newwords = urlParams.get('words');
+	var newwords = urlParams.get('words');
+	if (!newwords) {
+		newwords = 'Hello___Sir';
+	}
 	const gostring = newwords.split('___').join('\n');
 	var animStyle = urlParams.get('style');
 	// If no style is requested, pick one from this herearray
@@ -94,7 +101,7 @@ window.onload = function() {
 	
 	// Set up object to contain particles and set some default values
 	var particles = {},
-		particleIndex = 200,
+		particleIndex = 100,
 		settings = {
 			hasStarted: false,
 			density: 15,
@@ -148,13 +155,14 @@ window.onload = function() {
 			settings.fadeSpeed = 0;
 			break;
 		case "bounce":
+			particleIndex = 50;
 			settings.diefade = true;
 			settings.floorbounce = true;
 			settings.rotate = false;
 			settings.partLife = 280;
 			break;
 		case "stickmove":
-			particleIndex = 40;
+			particleIndex = 20;
 			settings.diefade = false;
 			settings.floorbounce = false;
 			settings.startingX = canvas.width / 1.45;
@@ -166,6 +174,7 @@ window.onload = function() {
 			settings.gravity = 0;
 			break;
 		case "bubbles":
+			particleIndex = 50;
 			settings.diefade = true;
 			settings.floorbounce = false;
 			settings.rotate = true;
@@ -184,6 +193,7 @@ window.onload = function() {
 			settings.popOut = true;
 			break;
 		case "arc":
+			particleIndex = 50;
 			settings.density = 12;
 			settings.diefade = true;
 			settings.floorbounce = true;
@@ -211,8 +221,8 @@ window.onload = function() {
 			settings.initvx = -2;
 			settings.initvyrnd = 5;
 			settings.initvy = 4;
-			settings.initryrnd = 27;
-			settings.initvr = 2;
+			settings.initryrnd = 17;
+			settings.initvr = 0;
 			break;
 		default:
 			settings.diefade = false;
